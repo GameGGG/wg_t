627388e898983bd82bb7fbaff64899916e06641b
ganlei <ganlei@baidu.com>
2019-04-16 20:59:07 +0800

    使用translation.js模块

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 const axios = require('axios');
-const _axios = axios.create({
-    headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
-});
+const { youdao, baidu, google } = require('translation.js');
+
+async function translate(word) {
+    console.log(word);
+    const gr = await google.translate(word);
+    const br = await baidu.translate(word);
+    const yr = await youdao.translate(word);
+
+    console.log('google: ', gr.result.join(' '));
+
+    console.log('baidu: ', br.result.join(' '));
+    
+    console.log('youdao: ', yr.result.join(' '));
+}
+
 
 const word = process.argv.slice(2);
-const baiduApi = 'https://fanyi.baidu.com/transapi';
-const youdaoApi = 'http://fanyi.youdao.com/translate';
-_axios.get(`${youdaoApi}?&doctype=json&type=ZH_CH2EN&i=${encodeURIComponent(word)}`).then(res => {
-    if (res.status === 200 && res.data.errorCode === 0) {
-        console.log('有道结果: ',res.data.translateResult[0][0].tgt);
-    }
-});
-_axios.get(`${baiduApi}?&from=auto&to=en&query=${encodeURIComponent(word)}`).then(res => {
-    if (res.status === 200 && res.data.status === 0) {
-        console.log('百度结果: ',res.data.data[0].dst);
-    }
-})
+
+translate(word[0]);
