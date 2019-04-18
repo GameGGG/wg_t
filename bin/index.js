46115da0de5b3c17e183e36745f874aa4a83445b
ganlei <ganlei@baidu.com>
2019-04-18 20:22:35 +0800

    add open browser

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,27 @@
 #!/usr/bin/env node
+const open = require('open');
 const { youdao, baidu, google } = require('translation.js');
+const urlRex = /^((http|ftp|https):\/\/)?[\w\-_]+(\.[\w\-\_]+)+([\w\-\.,@?\^=%&amp;:/~\+#]*[\w\-\@?\^=%&amp;/~\+#])?/
 
 async function translate(word) {
     if (!word) {
-        console.log('请输入翻译内容');
+        console.log('请输入内容');
         return;
     }
+
+    // 检测是否是本地文件或者是网址
+    if (urlRex.test(word)) {
+        if (!/^(http|https|ftp):\/\//.word) {
+            word = `http://${word}`;
+        }
+        open(word).then(res => {
+            console.log(`已打开网址：${word}`);
+        }).catch(err => {
+            console.log('打开网址失败');
+        });
+        return;
+    };
+
     const gr = await google.translate(word);
     const br = await baidu.translate(word);
     const yr = await youdao.translate(word);
