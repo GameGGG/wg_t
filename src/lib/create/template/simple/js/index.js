6657c825ae24482ec3ffca222ed8d63683c70341
white_g <mganxialei@gmail.com>
2019-05-25 22:16:54 +0800

    补充simple的template内容

---
+++ src/lib/create/template/simple/js/index.js
@@ -0,0 +1,8 @@
+console.log('Hello World');
+
+const body = document.querySelector('body');
+const h1 = document.createElement('h1');
+h1.innerHTML = 'Hello World';
+h1.style.textAlign = 'center';
+
+body.appendChild(h1);
