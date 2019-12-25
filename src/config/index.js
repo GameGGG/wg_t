0d5187de4d10e8940fb9790bde870da3d5470441
之味 <ganlei.gl@alibaba-inc.com>
2019-12-25 11:30:16 +0800

    屏蔽translate指令

--- src/config/index.js
+++ src/config/index.js
@@ -1,21 +1,21 @@
 const path = require('path');
 module.exports = [
-    {
-        command: 'translate [words...]',
-        alias: 't',
-        description: '翻译',
-        actionModule: 'translate',
-        options: [
-            {
-                argv: '-h, --help',
-                des: '翻译，使用translate模块进行翻译，帮助在命令行直接简单翻译'
-            },
-            {
-                argv: '-s, --search',
-                des: '在网页中搜索查找结果'
-            }
-        ]
-    },
+    // {
+    //     command: 'translate [words...]',
+    //     alias: 't',
+    //     description: '翻译',
+    //     actionModule: 'translate',
+    //     options: [
+    //         {
+    //             argv: '-h, --help',
+    //             des: '翻译，使用translate模块进行翻译，帮助在命令行直接简单翻译'
+    //         },
+    //         {
+    //             argv: '-s, --search',
+    //             des: '在网页中搜索查找结果'
+    //         }
+    //     ]
+    // },
     {
         command: 'create [name]',
         alias: 'c',
